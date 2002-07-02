369c31d8232edfbb9b0c8ffc3a8c0bec7ab97ff1
philburk <philburk@0f58301d-fd10-0410-b4af-bbb618454e57>
2002-07-02 05:08:13 +0000

    Added automated testing of all possible configurations.
    git-svn-id: https://subversion.assembla.com/svn/portaudio/portaudio/branches/v19-devel@305 0f58301d-fd10-0410-b4af-bbb618454e57

--- pa_tests/patest_wire.c
+++ pa_tests/patest_wire.c
@@ -43,25 +43,21 @@
 
 #define SAMPLE_RATE            (44100)
 
-/* Switch these USE flags between 0 and 1 for various test options. */
-#define USE_INTERLEAVED        (0)
+typedef struct WireConfig_s
+{
+    int isInputInterleaved;
+    int isOutputInterleaved;
+    int numInputChannels;
+    int numOutputChannels;
+    int framesPerCallback;
+} WireConfig_t;
+
 #define USE_FLOAT_INPUT        (1)
 #define USE_FLOAT_OUTPUT       (1)
-
-#define NUM_INPUT_CHANNELS     (1)
-#define NUM_OUTPUT_CHANNELS    (2)
-#define FRAMES_PER_CALLBACK      (0)
 
 #define OUTPUT_LATENCY_MSEC    (0)
 #define OUTPUT_LATENCY_FRAMES  (OUTPUT_LATENCY_MSEC * SAMPLE_RATE / 1000)
 
-#if USE_INTERLEAVED
-    #define FLAG_INTERLEAVED   (0)
-    #define CALLBACK_FUNCTION  wireCallback
-#else
-    #define FLAG_INTERLEAVED   (paNonInterleaved)
-    #define CALLBACK_FUNCTION  wireCallbackNonInterleaved
-#endif /* USE_INTERLEAVED */
 
 #if USE_FLOAT_INPUT
     #define INPUT_FORMAT  paFloat32
@@ -85,6 +81,8 @@ double gInOutScaler = 1.0;
 #define INPUT_DEVICE           (Pa_GetDefaultInputDevice())
 #define OUTPUT_DEVICE          (Pa_GetDefaultOutputDevice())
 
+static PaError TestConfiguration( WireConfig_t *config );
+
 static int wireCallback( void *inputBuffer, void *outputBuffer,
                          unsigned long framesPerBuffer,
                          PaTimestamp outTime, void *userData );
@@ -93,45 +91,21 @@ static int wireCallback( void *inputBuffer, void *outputBuffer,
 ** It may be called at interrupt level on some machines so don't do anything
 ** that could mess up the system like calling malloc() or free().
 */
+
 static int wireCallback( void *inputBuffer, void *outputBuffer,
                          unsigned long framesPerBuffer,
                          PaTimestamp outTime, void *userData )
 {
-    OUTPUT_SAMPLE *out = (OUTPUT_SAMPLE*)outputBuffer;
-    INPUT_SAMPLE *in = (INPUT_SAMPLE*)inputBuffer;
+    INPUT_SAMPLE *in;
+    OUTPUT_SAMPLE *out;
+    int inStride;
+    int outStride;
 
-    unsigned int i;
-    (void) outTime;
-    /* This may get called with NULL inputBuffer during initial setup. */
-    if( inputBuffer == NULL) return 0;
-
-    for( i=0; i<framesPerBuffer; i++ )
-    {
-        OUTPUT_SAMPLE outSample = CONVERT_IN_TO_OUT( *in++ );  /* left */
-        *out++ = outSample;
-
-#if ( NUM_INPUT_CHANNELS == 2 )
-        outSample = CONVERT_IN_TO_OUT( *in++ ); /* right input */
-#endif
-
-#if ( NUM_OUTPUT_CHANNELS == 2 )
-        *out++ = outSample;  /* right output */
-#endif
-    }
-
-    return 0;
-}
-
-static int wireCallbackNonInterleaved( void *inputBuffer, void *outputBuffer,
-                         unsigned long framesPerBuffer,
-                         PaTimestamp outTime, void *userData )
-{
-    OUTPUT_SAMPLE **outBuffers = (OUTPUT_SAMPLE**)outputBuffer;
-    INPUT_SAMPLE **inBuffers = (INPUT_SAMPLE**)inputBuffer;
     int inDone = 0;
     int outDone = 0;
-
-    unsigned int i, inChannel, outChannel;
+    WireConfig_t *config = (WireConfig_t *) userData;
+    unsigned int i;
+    int inChannel, outChannel;
     (void) outTime;
 
     /* This may get called with NULL inputBuffer during initial setup. */
@@ -140,17 +114,38 @@ static int wireCallbackNonInterleaved( void *inputBuffer, void *outputBuffer,
     inChannel=0, outChannel=0;
     while( !(inDone && outDone) )
     {
-        INPUT_SAMPLE *in = inBuffers[inChannel];
-        OUTPUT_SAMPLE *out = outBuffers[outChannel];
+        if( config->isInputInterleaved )
+        {
+            in = ((INPUT_SAMPLE*)inputBuffer) + inChannel;
+            inStride = config->numInputChannels;
+        }
+        else
+        {
+            in = ((INPUT_SAMPLE**)inputBuffer)[inChannel];
+            inStride = 1;
+        }
+
+        if( config->isOutputInterleaved )
+        {
+            out = ((OUTPUT_SAMPLE*)outputBuffer) + outChannel;
+            outStride = config->numOutputChannels;
+        }
+        else
+        {
+            out = ((OUTPUT_SAMPLE**)outputBuffer)[outChannel];
+            outStride = 1;
+        }
 
         for( i=0; i<framesPerBuffer; i++ )
         {
-            *out++ = CONVERT_IN_TO_OUT( *in++ );
+            *out = CONVERT_IN_TO_OUT( *in );
+            out += outStride;
+            in += inStride;
         }
 
-        if(inChannel < (NUM_INPUT_CHANNELS - 1)) inChannel++;
+        if(inChannel < (config->numInputChannels - 1)) inChannel++;
         else inDone = 1;
-        if(outChannel < (NUM_OUTPUT_CHANNELS - 1)) outChannel++;
+        if(outChannel < (config->numOutputChannels - 1)) outChannel++;
         else outDone = 1;
     }
     return 0;
@@ -160,22 +155,19 @@ static int wireCallbackNonInterleaved( void *inputBuffer, void *outputBuffer,
 int main(void);
 int main(void)
 {
-    PaStream *stream;
     PaError err;
-   
+    WireConfig_t CONFIG;
+    WireConfig_t *config = &CONFIG;
+    int configIndex = 0;;
+
     err = Pa_Initialize();
     if( err != paNoError ) goto error;
 
-    if( FLAG_INTERLEAVED == paNonInterleaved )
-    {
-        printf("PortAudio Test: NON interleaved!\n" );
-    }
-    printf("PortAudio Test: input channels = %d\n", NUM_INPUT_CHANNELS );
-    printf("PortAudio Test: output channels = %d\n", NUM_OUTPUT_CHANNELS );
-    printf("PortAudio Test: input format = %d\n", INPUT_FORMAT );
-    printf("PortAudio Test: output format = %d\n", OUTPUT_FORMAT );
-    printf("PortAudio Test: input device ID  = %d\n", INPUT_DEVICE );
-    printf("PortAudio Test: output device ID = %d\n", OUTPUT_DEVICE );
+    printf("Please connect audio signal to input and listen for it on output!\n");
+    printf("input format = %d\n", INPUT_FORMAT );
+    printf("output format = %d\n", OUTPUT_FORMAT );
+    printf("input device ID  = %d\n", INPUT_DEVICE );
+    printf("output device ID = %d\n", OUTPUT_DEVICE );
 
     if( INPUT_FORMAT == OUTPUT_FORMAT )
     {
@@ -190,43 +182,82 @@ int main(void)
         gInOutScaler = 32768.0;
     }
 
-    err = Pa_OpenStream(
-              &stream,
-              INPUT_DEVICE,
-              NUM_INPUT_CHANNELS,
-              INPUT_FORMAT | FLAG_INTERLEAVED,
-              0,               /* input latency */
-              NULL,
-              OUTPUT_DEVICE,
-              NUM_OUTPUT_CHANNELS,
-              OUTPUT_FORMAT | FLAG_INTERLEAVED,
-              OUTPUT_LATENCY_FRAMES,   /* output latency */
-              NULL,
-              SAMPLE_RATE,
-              FRAMES_PER_CALLBACK,            /* frames per buffer */
-              paClipOff,       /* we won't output out of range samples so don't bother clipping them */
-              CALLBACK_FUNCTION,
-              NULL );          /* no data */
-    if( err != paNoError ) goto error;
-    
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) goto error;
-    
-    printf("Full duplex sound test in progress.\n");
-    printf("Hit ENTER to exit test.\n");  fflush(stdout);
-    getchar();
-    
-    printf("Closing stream.\n");
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto error;
+    for( config->isInputInterleaved = 0; config->isInputInterleaved < 2; config->isInputInterleaved++ )
+    {
+        for( config->isOutputInterleaved = 0; config->isOutputInterleaved < 2; config->isOutputInterleaved++ )
+        {
+            for( config->numInputChannels = 1; config->numInputChannels < 3; config->numInputChannels++ )
+            {
+                for( config->numOutputChannels = 1; config->numOutputChannels < 3; config->numOutputChannels++ )
+                {
+                    for( config->framesPerCallback = 0; config->framesPerCallback < 65; config->framesPerCallback += 64 )
+                    {
+                        printf("-----------------------------------------------\n" );
+                        printf("Configuration #%d\n", configIndex++ );
+                        err = TestConfiguration( config );
+                        if( err != paNoError ) goto error;
+                    }
+               }
+            }
+        }
+    }
+
     Pa_Terminate();
     
     printf("Full duplex sound test complete.\n"); fflush(stdout);
+    
+    printf("Hit ENTER to quit.\n");  fflush(stdout);
+    getchar();
+
     return 0;
+
 error:
     Pa_Terminate();
     fprintf( stderr, "An error occured while using the portaudio stream\n" );
     fprintf( stderr, "Error number: %d\n", err );
     fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+    printf("Hit ENTER to quit.\n");  fflush(stdout);
+    getchar();
     return -1;
+}
+
+static PaError TestConfiguration( WireConfig_t *config )
+{
+    PaError err;
+    PaStream *stream;
+    printf("input %sinterleaved!\n", (config->isInputInterleaved ? " " : "NOT ") );
+    printf("output %sinterleaved!\n", (config->isOutputInterleaved ? " " : "NOT ") );
+    printf("input channels = %d\n", config->numInputChannels );
+    printf("output channels = %d\n", config->numOutputChannels );
+    printf("framesPerCallback = %d\n", config->framesPerCallback );
+
+    err = Pa_OpenStream(
+              &stream,
+              INPUT_DEVICE,
+              config->numInputChannels,
+              INPUT_FORMAT | (config->isInputInterleaved ? 0 : paNonInterleaved),
+              0,               /* input latency */
+              NULL,
+              OUTPUT_DEVICE,
+              config->numOutputChannels,
+              OUTPUT_FORMAT | (config->isOutputInterleaved ? 0 : paNonInterleaved),
+              OUTPUT_LATENCY_FRAMES,   /* output latency */
+              NULL,
+              SAMPLE_RATE,
+              config->framesPerCallback,            /* frames per buffer */
+              paClipOff,       /* we won't output out of range samples so don't bother clipping them */
+              wireCallback,
+              config );          /* user data */
+    if( err != paNoError ) goto error;
+    
+    err = Pa_StartStream( stream );
+    if( err != paNoError ) goto error;
+    
+    printf("Hit ENTER for next configuration.\n");  fflush(stdout);
+    getchar();
+    
+    printf("Closing stream.\n");
+    err = Pa_CloseStream( stream );
+error:
+    return err;
 }
