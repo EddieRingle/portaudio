aa1f2907585d86346ae732ef765472137ea40efd
rossbencina <rossbencina@0f58301d-fd10-0410-b4af-bbb618454e57>
2003-07-23 19:52:45 +0000

    switch off ASIO specific info with PA_NO_ASIO (thanks Bill Eldridge)
    git-svn-id: https://subversion.assembla.com/svn/portaudio/portaudio/branches/v19-devel@541 0f58301d-fd10-0410-b4af-bbb618454e57

--- pa_tests/pa_devs.c
+++ pa_tests/pa_devs.c
@@ -1,6 +1,9 @@
 /** @file pa_devs.c
     @brief List available devices, including device information.
 	@author Phil Burk http://www.softsynth.com
+
+    @note Define PA_NO_ASIO to compile this code on Windows without
+        ASIO support.
 */
 /*
  * $Id$
@@ -38,7 +41,9 @@
 #include "portaudio.h"
 
 #ifdef WIN32
+#ifndef PA_NO_ASIO
 #include "pa_asio.h"
+#endif
 #endif
 
 /*******************************************************************/
@@ -154,6 +159,7 @@ int main(void)
         printf( "Default high output latency = %8.3f\n", deviceInfo->defaultHighOutputLatency  );
 
 #ifdef WIN32
+#ifndef PA_NO_ASIO
 /* ASIO specific latency information */
         if( Pa_GetHostApiInfo( deviceInfo->hostApi )->type == paASIO ){
             long minLatency, maxLatency, preferredLatency, granularity;
@@ -170,6 +176,7 @@ int main(void)
             else
                 printf( "ASIO buffer granularity     = %ld\n", granularity  );
         }
+#endif /* !PA_NO_ASIO */
 #endif /* WIN32 */
 
         printf( "Default sample rate         = %8.2f\n", deviceInfo->defaultSampleRate );
