9e79e86789fc374496875a73c8ee20f0036a3c20
bjornroche <bjornroche@0f58301d-fd10-0410-b4af-bbb618454e57>
2006-04-22 14:32:23 +0000

    fixed some endianness problems for universal binary compilation on mac OS X.
    git-svn-id: https://subversion.assembla.com/svn/portaudio/portaudio/branches/v19-devel@991 0f58301d-fd10-0410-b4af-bbb618454e57

--- pa_common/pa_endianness.h
+++ pa_common/pa_endianness.h
@@ -54,38 +54,56 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-
-#if defined(PA_LITTLE_ENDIAN) || defined(PA_BIG_ENDIAN)
-    /* endianness define has been set externally, such as by autoconf */
-
-    #if defined(PA_LITTLE_ENDIAN) && defined(PA_BIG_ENDIAN)
-    #error both PA_LITTLE_ENDIAN and PA_BIG_ENDIAN have been defined externally to pa_endianness.h - only one endianness at a time please
-    #endif
-
-#else
-    /* endianness define has not been set externally */
-
-    /* set PA_LITTLE_ENDIAN or PA_BIG_ENDIAN by testing well known platform specific defines */
-
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(LITTLE_ENDIAN) || defined(__i386) || defined(_M_IX86)
-
-    #define PA_LITTLE_ENDIAN /* win32, assume intel byte order */
-
+/* If this is an apple, we need to do detect endianness this way */
+#if defined(__APPLE__)
+    /* we need to do some endian detection that is sensitive to harware arch */
+    #if defined(__LITTLE_ENDIAN__)
+       #if !defined( PA_LITTLE_ENDIAN )
+          #define PA_LITTLE_ENDIAN
+       #endif
+       #if defined( PA_BIG_ENDIAN )
+          #undef PA_BIG_ENDIAN
+       #endif
     #else
-		
-		 #define PA_BIG_ENDIAN
-
-#endif
-
-    #if !defined(PA_LITTLE_ENDIAN) && !defined(PA_BIG_ENDIAN)
-    /*
-     If the following error is raised, you either need to modify the code above
-     to automatically determine the endianness from other symbols defined on your
-     platform, or define either PA_LITTLE_ENDIAN or PA_BIG_ENDIAN externally.
-    */
-    #error pa_endianness.h was unable to automatically determine the endianness of the target platform
+       #if !defined( PA_BIG_ENDIAN )
+          #define PA_BIG_ENDIAN
+       #endif
+       #if defined( PA_LITTLE_ENDIAN )
+          #undef PA_LITTLE_ENDIAN
+       #endif
     #endif
+#else
+   /* this is not an apple, so first check the existing defines, and, failing that,
+      detect well-known architechtures. */
+
+   #if defined(PA_LITTLE_ENDIAN) || defined(PA_BIG_ENDIAN)
+       /* endianness define has been set externally, such as by autoconf */
+
+       #if defined(PA_LITTLE_ENDIAN) && defined(PA_BIG_ENDIAN)
+       #error both PA_LITTLE_ENDIAN and PA_BIG_ENDIAN have been defined externally to pa_endianness.h - only one endianness at a time please
+       #endif
+
+       /* endianness define has not been set externally */
+
+       /* set PA_LITTLE_ENDIAN or PA_BIG_ENDIAN by testing well known platform specific defines */
+
+       #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(LITTLE_ENDIAN) || defined(__i386) || defined(_M_IX86)
+          #define PA_LITTLE_ENDIAN /* win32, assume intel byte order */
+       #else
+          #define PA_BIG_ENDIAN
+       #endif
+
+       #if !defined(PA_LITTLE_ENDIAN) && !defined(PA_BIG_ENDIAN)
+       /*
+        If the following error is raised, you either need to modify the code above
+        to automatically determine the endianness from other symbols defined on your
+        platform, or define either PA_LITTLE_ENDIAN or PA_BIG_ENDIAN externally.
+       */
+       #error pa_endianness.h was unable to automatically determine the endianness of the target platform
+       #endif
     
+   #endif
+
 #endif
 
 /* PA_VALIDATE_ENDIANNESS compares the compile time and runtime endianness,
