8686beeb0d79d94731fbfc20299906382543be83
bjornroche <bjornroche@0f58301d-fd10-0410-b4af-bbb618454e57>
2006-07-05 16:38:39 +0000

    adds a compilation option to use non-atomic operations for setting and clearing flags. This is not SMP safe, but it probably won't lead to any disasters either.
    git-svn-id: https://subversion.assembla.com/svn/portaudio/portaudio/branches/v19-devel@1066 0f58301d-fd10-0410-b4af-bbb618454e57

--- src/hostapi/coreaudio/pa_mac_core_blocking.c
+++ src/hostapi/coreaudio/pa_mac_core_blocking.c
@@ -5,7 +5,12 @@
 #include "pa_mac_core_blocking.h"
 #include "pa_mac_core_internal.h"
 #include <assert.h>
-#include <libkern/OSAtomic.h>
+#ifdef MOSX_USE_NON_ATOMIC_FLAG_BITS
+# define OSAtomicOr32( a, b ) ( (*(b)) |= (a) )
+# define OSAtomicAnd32( a, b ) ( (*(b)) &= (a) )
+#else
+# include <libkern/OSAtomic.h>
+#endif
 
 /*
  * This fnuction determines the size of a particular sample format.
